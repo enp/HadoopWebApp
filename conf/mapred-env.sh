f426545f5fbe52e48381cb158cbf40d01a5ef402
Eugene Prokopiev <enp@itx.ru>
2021-12-03 13:53:15 +0300

    Revert "try to use map/reduce via yarn"
    This reverts commit 9c0aaeb1f1fd560b5ac3a89225fab9c368b9ccea.

--- conf/mapred-env.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-export HADOOP_JOB_HISTORYSERVER_OPTS="-Xms700m -Xmx8G"
--- conf/yarn-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-						
-
-
-export YARN_RESOURCEMANAGER_OPTS="-Xms1G -Xmx8G"
-export YARN_TIMELINESERVER_OPTS="-Xms700m -Xmx8G"
